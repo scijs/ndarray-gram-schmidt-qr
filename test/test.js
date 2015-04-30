7fd5485bc84979a180f00724de07d97861472f2d
Ricky Reusser <rsreusser@gmail.com>
2015-04-30 16:34:17 -0400

    Fix test case

--- test/test.js
+++ test/test.js
@@ -13,13 +13,15 @@ describe("Gram-Schmidt QR", function() {
   it('computes the in-place QR factorization of a square matrix',function() {
     var i,j;
     var n=3, m=3;
-    var A = ndarray(new Float64Array([1,2,3,4,5,6,7,8,9]), [n,m]);
+    var A = ndarray(new Float64Array([1,2,3,4.5,5,6,7,8,3]), [n,m]);
     var Q = pool.zeros( A.shape, A.dtype );
     var R = pool.zeros( A.shape, A.dtype );
     var QR = pool.zeros( A.shape, A.dtype );
     var diff = pool.zeros( A.shape, A.dtype );
 
-    qr(A, R);
+    var success = qr(A, R);
+
+    assert(success);
 
     // Confirm that all sub-diagonal entries are close to zero:
     for(i=1; i<n; i++) {
@@ -30,10 +32,10 @@ describe("Gram-Schmidt QR", function() {
 
     // Confirm that the matrices are basically equal:
     gemm( QR, A, R ); 
-    A = ndarray(new Float64Array([1,2,3,4,5,6,7,8,9]), [n,m]); // <-- the original has been overwritten, so reassign
+    A = ndarray(new Float64Array([1,2,3,4.5,5,6,7,8,3]), [n,m]); // <-- the original has been overwritten, so reassign
     ops.sub( diff, QR, A );
-    var err = ops.norm2( diff ); // <-- not to be confused with the 2-norm of a matrix, but it's basically good enough
-    assert.closeTo( err, 0, 1e-8 );
+    var err2 = ops.norm2( diff ); // <-- not to be confused with the 2-norm of a matrix, but it's basically good enough
+    assert.closeTo( err2, 0, 1e-8 );
 
   });
 
