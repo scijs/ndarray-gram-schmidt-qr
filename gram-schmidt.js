7b514e3041607478ae35b7e5cd357d5d430660e2
Ricky Reusser <rsreusser@gmail.com>
2015-04-30 16:09:14 -0400

    Initial commit

---
+++ gram-schmidt.js
@@ -0,0 +1,68 @@
+'use strict';
+
+
+var assert = require('assert');
+
+var ndarray = require('ndarray'),
+    ops = require('ndarray-ops'),
+    cwise = require('cwise');
+
+
+var innerproduct = cwise({
+  args:["array", "array"],
+  pre: function() {
+    this.sum = 0;
+  },
+  body: function(a,b) {
+    this.sum += a * b;
+  },
+  post: function() {
+    return this.sum;
+  }
+});
+
+
+var axpy = cwise({
+  args:["scalar", "array", "array"],
+  body: function(alpha, x, y) {
+    y += alpha * x;
+  }
+});
+
+module.exports = function modifiedGramSchmidtQR( A, R ) {
+
+  var i,j, rii, vi, qi, vj, rij;
+
+  assert(A.dimension == 2);
+
+  var m = A.shape[0];
+  var n = A.shape[1];
+
+  for( i=0; i<n; i++ ) {
+
+    // vi = ai
+    vi = A.pick( null, i );
+
+    // rii = ||vi||
+    rii = ops.norm2( vi );
+    if( rii===0 ) { return false; } // Bail if rii == 0
+    R.set(i, i, rii);
+
+    // qi = vi/rii
+    qi = A.pick( null, i );
+    ops.assign( qi, vi );
+    ops.divseq( qi, rii );
+
+    for( j=i+1; j<n; j++ ) {
+      //rij = qi' * vj
+      vj = A.pick( null, j );
+      rij = innerproduct( qi, vj );
+      R.set( i, j, rij );
+
+      // vj = vj - rij * qi
+      axpy( -rij, qi, vj );
+    }
+  }
+
+  return true;
+};
---
+++ test/test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var qr = require('../gram-schmidt.js'),
+    assert = require('chai').assert,
+    ndarray = require('ndarray'),
+    pool = require('ndarray-scratch'),
+    gemm = require("ndgemm"),
+    ops = require('ndarray-ops');
+
+
+describe("Gram-Schmidt QR", function() {
+
+  it('computes the in-place QR factorization of a square matrix',function() {
+    var i,j;
+    var n=3, m=3;
+    var A = ndarray(new Float64Array([1,2,3,4,5,6,7,8,9]), [n,m]);
+    var Q = pool.zeros( A.shape, A.dtype );
+    var R = pool.zeros( A.shape, A.dtype );
+    var QR = pool.zeros( A.shape, A.dtype );
+    var diff = pool.zeros( A.shape, A.dtype );
+
+    qr(A, R);
+
+    // Confirm that all sub-diagonal entries are close to zero:
+    for(i=1; i<n; i++) {
+      for(j=0; j<i; j++) {
+        assert.closeTo( R.get(i,j), 0, 1e-8 );
+      }
+    }
+
+    // Confirm that the matrices are basically equal:
+    gemm( QR, A, R ); 
+    A = ndarray(new Float64Array([1,2,3,4,5,6,7,8,9]), [n,m]); // <-- the original has been overwritten, so reassign
+    ops.sub( diff, QR, A );
+    var err = ops.norm2( diff ); // <-- not to be confused with the 2-norm of a matrix, but it's basically good enough
+    assert.closeTo( err, 0, 1e-8 );
+
+  });
+
+});
